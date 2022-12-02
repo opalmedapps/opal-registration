e5f76d66154e3eaf1873273ca688140779328555
Limin Liu <limin.liu@muhc.mcgill.ca>
2022-12-02 14:11:49 +0000

    Feat add MRN login support for registration UI (QSCCD-172)
    See merge request opalmedapps/opal-registration#27

--- app/listener/userAuthorization.service.js
+++ app/listener/userAuthorization.service.js
@@ -7,29 +7,29 @@
 
     function userAuthorizationService() {
         /**
-            @ngdoc property
-            @name  MUHCApp.service.#userCode
-            @propertyOf MUHCApp.service:userAuthorizationService
-            @description code property
+         @ngdoc property
+         @name  MUHCApp.service.#userCode
+         @propertyOf MUHCApp.service:userAuthorizationService
+         @description code property
          **/
         var userCode = '';
 
 
         /**
-            @ngdoc property
-            @name  MUHCApp.service.#userBranchName
-            @propertyOf MUHCApp.service:userAuthorizationService
-            @description code property
+         @ngdoc property
+         @name  MUHCApp.service.#userBranchName
+         @propertyOf MUHCApp.service:userAuthorizationService
+         @description code property
          **/
         var userBranchName = '';
 
         /**
-           @ngdoc property
-           @name  MUHCApp.service.#userRAMQ
-           @propertyOf MUHCApp.service:userAuthorizationService
-           @description code property
-        **/
-        var userRAMQ = '';
+         @ngdoc property
+         @name  MUHCApp.service.#userSalt
+         @propertyOf MUHCApp.service:userAuthorizationService
+         @description code property
+         **/
+        var userSalt = '';
 
         /**
          @ngdoc property
@@ -40,26 +40,26 @@
         var hospitalCode = '';
 
         return {
-            
+
             /**
-                @ngdoc method
-                @name setUserData
-                @methodOf MUHCApp.service:userAuthorizationService
-                @description Sets all the user authorization.
-            **/
-            setUserData: function (code, ramq, hospitalcode) {
+             @ngdoc method
+             @name setUserData
+             @methodOf MUHCApp.service:userAuthorizationService
+             @description Sets all the user authorization.
+             **/
+            setUserData: function (code, salt, hospitalcode) {
                 userCode = code;      // Registration code
-                userRAMQ = ramq;    //RAMQ
+                userSalt = salt;    //RAMQ or MRN
                 hospitalCode = hospitalcode;    //hospitalcode
             },
 
             /**
-                @ngdoc method
-                @name setFirebaseBranchName
-                @methodOf MUHCApp.service:userAuthorizationService
-                @param {String} branchName
-                @description Sets all the user authorization.
-            **/
+             @ngdoc method
+             @name setFirebaseBranchName
+             @methodOf MUHCApp.service:userAuthorizationService
+             @param {String} branchName
+             @description Sets all the user authorization.
+             **/
             setUserBranchName: function (branchName) {
                 userBranchName = branchName;    // Firebase branch name
             },
@@ -76,47 +76,48 @@
             },
 
             /**
-               @ngdoc method
-               @name getUserBranchName
-               @methodOf MUHCApp.service:userAuthorizationService
-               @returns {string} Returns Firebase branch name.
-           **/
+             @ngdoc method
+             @name getUserBranchName
+             @methodOf MUHCApp.service:userAuthorizationService
+             @returns {string} Returns Firebase branch name.
+             **/
             getUserBranchName: function () {
                 return userBranchName;
 
             },
 
             /**
-               @ngdoc method
-               @name getUserRAMQ
-               @methodOf MUHCApp.service:userAuthorizationService
-               @returns {string} Returns Firebase branch name.
-           **/
-            getUserRAMQ: function () {
-                return userRAMQ;
+             @ngdoc method
+             @name getUserSalt
+             @methodOf MUHCApp.service:userAuthorizationService
+             @returns {string} Returns Firebase branch name.
+             **/
+            getUserSalt: function () {
+                return userSalt;
 
             },
-            
+
             /**
-                @ngdoc method
-                @name getuniqueCode
-                @methodOf MUHCApp.service:userAuthorizationService
-                @returns {string} Returns unique ID.
-            **/
+             @ngdoc method
+             @name getuniqueCode
+             @methodOf MUHCApp.service:userAuthorizationService
+             @returns {string} Returns unique ID.
+             **/
             getuserCode: function () {
                 return userCode;
             },
-            
+
             /**
-                @ngdoc method
-                @name clearuserAuthorizationInformation
-                @methodOf MUHCApp.service:userAuthorizationService
-                @description Clears service
-            **/
-            clearuserAuthorizationInfomation: function () {
+             @ngdoc method
+             @name clearUserAuthorizationInformation
+             @methodOf MUHCApp.service:userAuthorizationService
+             @description Clears service
+             **/
+            clearUserAuthorizationInfomation: function () {
                 userCode = '';
-                userRAMQ = '';
+                userSalt = '';
                 userBranchName = '';
+                hospitalCode = '';
             }
         }
     }
