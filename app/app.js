39e745133fb05febc93043ee0d545bc43b908af6
Stacey Beard <staceybeard10@gmail.com>
2025-01-24 13:57:56 -0500

    fix: remove unnecessary brands icons import

--- app/app.js
+++ app/app.js
@@ -21,7 +21,6 @@ import { initializeApp } from 'firebase/app';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
 
 // Project CSS
 import '../css/components.css';
