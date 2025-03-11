457f9f0a4950a0c2d8709b70c300b1545e43ee97
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-11 15:21:01 +0000

    Merge remote-tracking branch 'origin/main' into QSCCD-1842-extra
    (cherry picked from commit c003fd340e1534e9d878c125faf5f7c4dce04d45)
    
    Co-authored-by: BenoitCharb <benoit.charbonneau01@gmail.com>

--- app/components/registration/secure/secure.controller.js
+++ app/components/registration/secure/secure.controller.js
@@ -143,7 +143,6 @@ import * as zxcvbnFrPackage from '@zxcvbn-ts/language-fr';
 
             //Variable to set field status and message.
             vm.passwordFormat = { status: null, message: null };
-
             vm.formData.passwordMeter = $scope.passwordStrength;
             if (vm.parent.isEmpty(vm.formData.formFieldsData.password)) {
                 vm.formData.passwordFormat.status = vm.parent.STATUS_INVALID;
@@ -153,6 +152,11 @@ import * as zxcvbnFrPackage from '@zxcvbn-ts/language-fr';
                 if (vm.formData.formFieldsData.password.length < 10) {
                     vm.formData.passwordFormat.status = vm.parent.STATUS_INVALID;
                     vm.formData.passwordFormat.message = $filter('translate')('SECURE.FIELDERRORMESSAGES.SHORTPASSWORDLENGTH');
+                    vm.formData.passwordMeter = $scope.passwordStrength >= minPasswordStrength ? minPasswordStrength - 1 : $scope.passwordStrength;
+                    return;
+                } else if (vm.passwordContainsDomainName(vm.formData.formFieldsData.password)) {
+                    vm.formData.passwordFormat.status = vm.parent.STATUS_INVALID;
+                    vm.formData.passwordFormat.message =$filter('translate')('SECURE.FIELDERRORMESSAGES.PASSWORDINVALIDDOMAINNAME');
                     vm.formData.passwordMeter = $scope.passwordStrength >= minPasswordStrength ? minPasswordStrength - 1 : $scope.passwordStrength;
                     return;
                 } else if (vm.passwordContainsMRNorRAMQ(vm.formData.formFieldsData.password)){
@@ -185,8 +189,7 @@ import * as zxcvbnFrPackage from '@zxcvbn-ts/language-fr';
                     vm.formData.passwordFormat.message = $filter('translate')('SECURE.FIELDERRORMESSAGES.PASSWORDINVALIDLOWERCASE');
                     vm.formData.passwordMeter = $scope.passwordStrength >= minPasswordStrength ? minPasswordStrength - 1 : $scope.passwordStrength;
                     return;
-                }
-                else if (vm.formData.formFieldsData.password.search(/\W|_{1}/) === -1) {
+                } else if (vm.formData.formFieldsData.password.search(/\W|_{1}/) === -1) {
                     vm.formData.passwordFormat.status = vm.parent.STATUS_INVALID;
                     vm.formData.passwordFormat.message = $filter('translate')('SECURE.FIELDERRORMESSAGES.PASSWORDINVALIDSPECIALCHAR');
                     vm.formData.passwordMeter = $scope.passwordStrength >= minPasswordStrength ? minPasswordStrength - 1 : $scope.passwordStrength;
@@ -206,6 +209,11 @@ import * as zxcvbnFrPackage from '@zxcvbn-ts/language-fr';
                     }
                 }
             }
+        }
+        // Function that limits the user from entering the Opal domain name in their passowrd 
+        vm.passwordContainsDomainName = function(password) {
+            var opalRegex = /[0o@&]p[a@&4][l1!]/;   // edge cases for the word opal
+            return opalRegex.test(password.toLowerCase());     // returns true if one of the case is detected 
         }
         // Function that limits the user from entering their MRN or RAMQ in the password 
         vm.passwordContainsMRNorRAMQ = function (password) {
