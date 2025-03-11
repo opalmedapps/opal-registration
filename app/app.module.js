27c894214515e6c693a4c7e7ac60612ad96bf33f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-11 15:05:23 +0000

    Merge branch 'staging' into qsccd-177
    # Conflicts:
    #	app/app.module.js
    #	index.html
    #	translate/en.json
    #	translate/fr.json
    
    
    (cherry picked from commit 9b6062610941e084daed7143e4f9f081981f5754)
    
    Co-authored-by: Limin <llmgd@hotmail.com>

--- app/app.module.js
+++ app/app.module.js
@@ -126,7 +126,18 @@
                         },
                         footer: footer
                     }
+                })
 
+                // User account verification page.
+                .state('form.verification', {
+                    url: '/verification',
+                    views: {
+                        header: header,
+                        content: {
+                            component: 'verificationComponent'
+                        },
+                        footer: footer
+                    }
                 })
                
                 // User secure information page.
