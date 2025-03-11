aa5b7ac4190fcdb9a199f77f553217b2d7397352
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-11 15:18:26 +0000

    ci: copy .htaccess for deployment
    (cherry picked from commit 2623268925c141b348139909e039370b40bf9e87)
    
    Co-authored-by: Stacey Beard <staceybeard10@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const CopyPlugin = require("copy-webpack-plugin");
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
@@ -84,5 +85,11 @@ module.exports = {
             // title: 'Opal Registration',
         }),
         new FaviconsWebpackPlugin('./images/favicons/favicon-source.png'),
+        new CopyPlugin({
+            patterns: [
+                // File copied for web deployment
+                ".htaccess",
+            ],
+        }),
     ],
 };
