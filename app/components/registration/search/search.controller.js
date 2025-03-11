e7ddee4a61dd5eed894bca2fd38ca8e382903b05
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-11 15:08:53 +0000

    Merge branch 'staging' into QSCCD-172.LL.mrn-login
    (cherry picked from commit b0d0915d15d18de1ba5dcc10e6bc4fb69aba06bb)
    
    Co-authored-by: Limin <llmgd@hotmail.com>

--- app/components/registration/search/search.controller.js
+++ app/components/registration/search/search.controller.js
@@ -215,14 +215,12 @@
                 else {
 
                     // Call function to display error modal box.
-                    var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
-                    vm.parent.displayError(errorModalPage);
+                    vm.errorPopup();
                 }
             }).catch(function (error) {
 
                 // Call function to display error modal box.
-                var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
-                vm.parent.displayError(errorModalPage);
+                vm.errorPopup();
             });
 
         }
@@ -244,16 +242,14 @@
                     else {
 
                         // Call function to display error modal box.
-                        var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
-                        vm.parent.displayError(errorModalPage);
+                        vm.errorPopup();
                     }
 
                 })
                 .catch(function (error) {
                     
                     // Call function to display error modal box.
-                    var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
-                    vm.parent.displayError(errorModalPage);
+                    vm.errorPopup();
                 });
 
         }
@@ -276,36 +272,40 @@
                     else {
                      
                         // Call function to display error modal box.
-                        var errorModalPage = 'app/components/registration/shared/modalBox/somethingWentWrongError.html';
-                        vm.parent.displayError(errorModalPage);
+                        vm.errorPopup();
                     }
                 })
                 .catch(function (error) {
 
                     // Call function to display error modal box.
-                    var errorModalPage = 'app/components/registration/shared/modalBox/somethingWentWrongError.html';
-                    vm.parent.displayError(errorModalPage);
+                    vm.errorPopup();
                 });
         }
 
         // Method to call service to check valid input.
         vm.validInputs = function (requestObject) {
-            
-            // Parameter object
-            var parameters = {
-                'FirebaseBranchName': requestObject,
-                'RegistrationCode': vm.formData.formFieldsData.registrationCode,
-                'RAMQ': vm.formData.formFieldsData.ramq
-            };
 
             // Listener service call.
-            requestToListener.sendRequestWithResponse('ValidateInputs', { Fields: parameters })
+            const request = {
+                method: 'get',
+                url: `/api/registration/${vm.formData.formFieldsData.registrationCode}/`,
+            };
+
+            requestToListener.apiRequest(request, vm.formData.selectedLanguage)
                 .then(function (response) {
-                    var result = response.Data[0].Result.split(":");
-                    
-                    if (result[0] == 'SUCCESS') {
+
+                    if (response?.status_code == 200) {
                         // Call function to get user name.
-                        vm.formData.userName = result[1];
+                        const patient = response.data?.patient;
+                        const institution = response.data?.institution;
+
+                        if (patient &&  institution) {
+                            vm.formData.userName = `${patient?.first_name} ${patient?.last_name}`;
+                        } else {
+                            vm.formData.userName = `Not found`;
+                            var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
+                            vm.parent.displayError(errorModalPage);
+                        }
 
                         vm.retrieveTermsOfUsePDF()
                             .then(function () {
@@ -323,11 +323,9 @@
                             // Call function to reset value of every text fields.
                             vm.resetFields();
                         });
-                    }
-                    else {
+                    } else {
                         // Call function to display error modal box.
-                        var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
-                        vm.parent.displayError(errorModalPage);
+                        vm.errorPopup();
                     }
 
                 })
@@ -337,8 +335,7 @@
                     vm.formData.displaySpinner = true;
 
                     // Call function to display error modal box.
-                    var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
-                    vm.parent.displayError(errorModalPage);
+                    vm.errorPopup();
 
                     // Call function to reset value of every text fields.
                     vm.resetFields();
@@ -347,34 +344,32 @@
     
         // Function to load security questions list on service call.
         vm.getSecurityQuestionList = function () {
-
-            // Parameter
-            var parameters = vm.formData.formFieldsData.ramq;
             
             // Listener service call.
-            requestToListener.sendRequestWithResponse('SecurityQuestionsList', { Fields: parameters })
+            requestToListener.apiRequest(apiConstants.ROUTES.QUESTIONS, vm.formData.selectedLanguage)
                 .then(function (response) {
 
                     // assing response to temporary variable.
-                    var SecurityQuestionsList = response.Data[0];
+                    let securityQuestions = response?.data?.results;
+
+                    vm.formData.securityQuestionList_EN = [];
+                    vm.formData.securityQuestionList_FR = [];
 
                     // Check length of the variable
-                    if (SecurityQuestionsList.length > 1) {
-
+                    if (securityQuestions?.length > 1) {
                         // Define loop for passing the value of securityquestions.
-                        for (var i = 0; i < SecurityQuestionsList.length; i++) {
-
+                        securityQuestions.forEach((question) => {
                             // Assing in JSON format
-                            vm.formData.securityQuestionList_EN[i] = {
-                                "id": SecurityQuestionsList[i].SecurityQuestionSerNum,
-                                "value": SecurityQuestionsList[i].QuestionText_EN
-                            }
+                            vm.formData.securityQuestionList_EN.push({
+                                "id": question.id,
+                                "value": question.title_en,
+                            })
 
-                            vm.formData.securityQuestionList_FR[i] = {
-                                "id": SecurityQuestionsList[i].SecurityQuestionSerNum,
-                                "value": SecurityQuestionsList[i].QuestionText_FR
-                            }
-                        }
+                            vm.formData.securityQuestionList_FR.push({
+                                "id": question.id,
+                                "value": question.title_fr,
+                            })
+                        })
 
                         // Check the default selected language.
                         if (vm.formData.selectedLanguage == 'en')
@@ -389,18 +384,15 @@
                             $location.path('/form/secureInformation');
                         });
 
-                    }
-                    else {
+                    } else {
                         // Call function to display error modal box.
-                        var errorModalPage = 'app/components/registration/shared/modalBox/contactUsError.html';
-                        vm.parent.displayError(errorModalPage);
+                        vm.errorPopup();
                     }
                 })
                 .catch(function (error) {
                     
                     // Call function to display error modal box.
-                    var errorModalPage = 'app/components/registration/shared/modalBox/notFoundError.html';
-                    vm.parent.displayError(errorModalPage);
+                    vm.errorPopup();
 
                 });
         }
@@ -444,10 +436,14 @@
                     // Hide display spinner after all request get response.
                     vm.formData.displaySpinner = true;
 
-                    var errorModalPage = 'app/components/registration/shared/modalBox/contactUsError.html';
-                    vm.parent.displayError(errorModalPage, "unsuccessfulRegistration");
+                    vm.errorPopup();
                 });
             }
+        }
+
+        vm.errorPopup = function() {
+            const errorModalPage = 'app/components/registration/shared/modalBox/contactUsError.html';
+            vm.parent.displayError(errorModalPage, "unsuccessfulRegistration");
         }
     };
 })();
