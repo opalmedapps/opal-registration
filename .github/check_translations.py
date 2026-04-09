51bffe3e4959a70a31136fe1a992f0fb27abbdff
Matthias Schoettle <git@mattsch.com>
2026-04-09 10:33:23 -0400

    chore: improve translation check to include nested keys (#394)

--- .github/check_translations.py
+++ .github/check_translations.py
@@ -5,11 +5,27 @@
 import json
 import sys
 from pathlib import Path
+from typing import Any
 
 MASTER_LANGUAGE = 'en'
 TRANSLATION_ROOT = Path('translate/')
 
 errors = []
+
+def check_keys(first: dict[str, Any], second: dict[str, Any], language_file: Path, key: str = '') -> None:
+    missing = first.keys() - second.keys()
+    additional = second.keys() - first.keys()
+    key = f' (within key: {key})' if key else ''
+    if missing:
+        errors.append(f'{language_file}{key}: missing keys: {missing}')
+
+    if additional:
+        errors.append(f'{language_file}{key}: additional keys: {additional}')
+
+    for key in first.keys():
+        if isinstance(first[key], dict):
+            check_keys(first[key], second[key], language_file, key)
+
 
 print(f'comparing language files in {TRANSLATION_ROOT}...')
 
@@ -21,13 +37,7 @@ for language_file in list(TRANSLATION_ROOT.glob('*.json')):
         print(f'loading and comparing {language_file.name}...')
         language = json.loads(language_file.read_text())
 
-        missing = master.keys() - language.keys()
-        additional = language.keys() - master.keys()
-        if missing:
-            errors.append(f'{language_file}: missing keys: {missing}')
-
-        if additional:
-            errors.append(f'{language_file}: additional keys: {additional}')
+        check_keys(master, language, language_file)
 
 print('')
 
